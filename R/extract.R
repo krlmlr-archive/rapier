c1d4c2a9b819bec7b3fc370bfb19d0557fe5ffa5
Kirill Müller <krlmlr+r@mailbox.org>
2017-01-10 17:53:59 +0100

    add top-level caption

--- R/extract.R
+++ R/extract.R
@@ -1,6 +1,7 @@
 extract_api <- function(pkg = ".") {
+  pkg <- pkgload::as.package(pkg)
   loaded <- pkgload::load_all(pkg = pkg, export_all = FALSE)
-  structure(list(loaded = loaded), class = "rapier")
+  structure(list(loaded = loaded, name = pkg$package), class = "rapier")
 }
 
 #' @export
@@ -11,14 +12,19 @@ print.rapier <- function(x, ...) {
 #' @export
 format.rapier <- function(x, ...) {
   c(
+    format_caption("API for ", x$name, " package", level = 1),
     add_caption("Exports", format_exports(x))
   )
+}
+
+format_caption <- function(..., level = 2) {
+  c(paste0(paste(rep("#", level), collapse = ""), " ", ...), "")
 }
 
 add_caption <- function(caption, x) {
   if (length(x) == 0L) {
     character()
   } else {
-    c(paste0("# ", caption), "", x)
+    c(format_caption(caption), x)
   }
 }
